66bbc70c7e3fcd7a23d4dc11d8cf6b4cf2a980b2
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2023-05-15 21:07:03 -0300

    [UPDATE] - CODE
    Script sem o modelo treinado.

---
+++ plant.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+while menu != "0":
+    carrega_imagem = input ("Digite o nome da planta, Ex (imagem.jpg):")
+
+    # Carregar modelo de detecção de doenças em plantas
+    modelo = tf.keras.models.load_model('modelo.h5')
+    # Ler imagem da planta
+    imagem = cv2.imread(carrega_imagem)
+    # Redimensionar imagem para entrada do modelo
+    imagem_redimensionada = cv2.resize(imagem, (224, 224))
+    # Normalizar imagem
+    imagem_normalizada = imagem_redimensionada / 255.0
+    # Adicionar dimensão de lote à imagem
+    imagem_normalizada = np.expand_dims(imagem_normalizada, axis=0)
+
+    # Executar modelo na imagem
+    resultado = modelo.predict(imagem_normalizada)
+    # Imprimir resultado
+        if resultado[0] < 0.5:
+     print('Planta saudável')
+        else:
+        print('Planta doente')
